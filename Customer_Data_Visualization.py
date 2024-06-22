fb7e7d0257eeb32626c098b87d1ba3b685620253
Michelle Mu <michellemjx@gmail.com>
2024-06-22 11:37:12 -0700

    Refactored code into functions for readability and reusability.

--- Customer_Data_Visualization.py
+++ Customer_Data_Visualization.py
@@ -17,8 +17,11 @@ from datetime import date
 # DONE make efficient with ws
 # DONE rename file
 # TODO try out df.aggregate
-# TODO def visulaize(col_name, is_month, chart_type)
-# TODO edit functions to include 'if is_month'
+# DONE def visulaize(col_name, is_month, chart_type)
+# DONE edit functions to include 'if is_month'
+# TODO save into different worksheets
+#   line 50 add new arg into for loop
+#   format_data() wb to ws
 
 
 # STEP BY STEP
@@ -43,96 +46,114 @@ from datetime import date
 # user_follow_behavior_data   uid, following, follow_by
 # heuristic_user_data         
 
-filepath = r'C:\Users\miche\OneDrive\Documents\CodingwDad\dad_intern\py-xl-chart.xlsx'
-dict_df = pd.read_excel(io=filepath, sheet_name=['Data'], usecols=[1,36,37]) # import xlsx file, Data sheet, columns for mode, fwcreatedate, and nettype
-df = dict_df['Data']
+def main():
+    filepath = r'C:\Users\miche\OneDrive\Documents\CodingwDad\dad_intern\py-xl-chart.xlsx'
+    df = import_data(filepath)
+    for col_name, output_file in [
+        ('FwCreateDate', 'dv_intern_daily.xlsx'),
+        ('FwCreateMonth', 'dv_intern_month.xlsx'),
+    ]:
+        wb = prepare_wb()
+        format_data(df, wb, col_name)
+        linechart_fw_creation_date = draw_line_chart(wb)
+        save_chart(wb, linechart_fw_creation_date, output_file)
+    
 
-df['FwCreateMonth'] = df['FwCreateDate'].str.slice(0,7)
 
-# Creates new workbook and retitles ws1 to 'Raw', creates 2 other sheets
-data_v = Workbook()
-ws1 = data_v.active
-ws1.title = 'Raw'
-ws2 = data_v.create_sheet('Formatted')
-ws3 = data_v.create_sheet('Visualization')
+def import_data(filepath):
+    dict_df = pd.read_excel(io=filepath, sheet_name=['Data'], usecols=[1,36,37]) # import xlsx file, Data sheet, columns for mode, fwcreatedate, and nettype
+    df = dict_df['Data']
 
-# reset worksheet variables to reference sheets
-ws1 = data_v['Raw']
-ws2 = data_v['Formatted']
-ws3 = data_v['Visualization']
+    df['FwCreateMonth'] = df['FwCreateDate'].str.slice(0,7)
 
-# 3 seperate columns for mode, fwcreatedate, and nettype
-mode = df.loc[:, 'Mode']
-# fw_create_date = df.loc[:, 'FwCreateDate']
-fw_create_date = df.loc[:, 'FwCreateMonth']
-net_type = df.loc[:,'NetType']
+    return df
 
-# creates sorted_count_date list within list [date, count], adds header in formatted_date_data
-count_date = Counter(list(fw_create_date))
-count_date.pop(np.nan, None) # pop np.nan from data, if nonexistent then do nothing
 
-# dictionary comp ONLY if the data is in YYYY-mm, NOT YYYY-mm-dd. Assigning false day value to YYYY-mm to use date function later
-if list(count_date.keys())[0].count('-') == 1:
-    count_date = {
-     (k + '-01') : v # what we are doing
-     for k, v in count_date.items() # for which items
-     if k is not np.nan # conditional # use is not for np.nan because we don't know if unknowns == unknowns, but we DO know if something IS NOT unknown
-     }
-list_count_date = count_date.items()
+def prepare_wb():
+    # Creates new workbook and retitles ws1 to 'Raw', creates 2 other sheets
+    wb = Workbook()
+    ws = wb.active
+    ws.title = 'Formatted'
+    return wb
 
-sorted_count_date = sorted([[date(*[int(item) for item in k.split('-')]),v] #what we are doing to the items
+
+def format_data(df, wb, col_name):
+    '''This function creates a visualization for date data'''
+    
+    # 3 seperate columns for mode, fwcreatedate, and nettype
+    mode = df.loc[:, 'Mode']
+    fw_create_date = df.loc[:, col_name]
+    net_type = df.loc[:,'NetType']
+
+    # creates sorted_count_date list within list [date, count], adds header in formatted_date_data
+    count_date = Counter(list(fw_create_date))
+    count_date.pop(np.nan, None) # pop np.nan from data, if nonexistent then do nothing
+    # definition of when is_month is True
+    is_month = list(count_date.keys())[0].count('-') == 1
+    # what to do when is_month is True
+    if is_month:
+        count_date = {
+            (k + '-01') : v # what we are doing
+            for k, v in count_date.items() # for which items
+            if k is not np.nan # conditional # use is not for np.nan because we don't know if unknowns == unknowns, but we DO know if something IS NOT unknown
+            }
+    # list_count_date = count_date.items()
+        
+    sorted_count_date = sorted([[date(*[int(item) for item in k.split('-')]),v] #what we are doing to the items
                             for k,v in count_date.items() #where we find the items from
                             if k is not np.nan] #conditional filter
                             )
-count_date_headers = [['Date','Count']]
-formatted_date_data = count_date_headers + sorted_count_date
+    count_date_headers = [['Date','Count']]
+    formatted_date_data = count_date_headers + sorted_count_date
+    # return formatted_date_data
 
-# appends formatted_date_data to ws2 only if ws2 is empty (to stop it from appending again every time the code is run)
-if ws2['A1'].value == None: # check ws2 is empty
-    for row in formatted_date_data:
-        ws2.append(row)
+    ws2 = wb['Formatted']
+    # appends formatted_date_data to ws2 only if ws2 is empty (to stop it from appending again every time the code is run)
+    if ws2['A1'].value == None: # check ws2 is empty
+        for row in formatted_date_data:
+            ws2.append(row)
+        
 
-# Line Chart for Frequency of Firewall Creation by Date
-linechart_fw_creation_date = LineChart()
-linechart_fw_creation_date.title = 'Frequency of Firewall Creation by Date'
-linechart_fw_creation_date.style = 12
-linechart_fw_creation_date.y_axis.title = 'Firewalls Created'
-linechart_fw_creation_date.y_axis.crossAx = 500 # wth does this even mean
-linechart_fw_creation_date.x_axis = DateAxis(crossAx=100)
-linechart_fw_creation_date.x_axis.number_format = 'd-mmm'
-linechart_fw_creation_date.x_axis.majorTimeUnit = 'days'
-linechart_fw_creation_date.x_axis.title = 'Date'
+def draw_line_chart(wb):
+    # Line Chart for Frequency of Firewall Creation by Date
+    linechart_fw_creation_date = LineChart()
+    linechart_fw_creation_date.title = 'Frequency of Firewall Creation by Date'
+    linechart_fw_creation_date.style = 12
+    linechart_fw_creation_date.y_axis.title = 'Firewalls Created'
+    linechart_fw_creation_date.y_axis.crossAx = 500 # wth does this even mean
+    linechart_fw_creation_date.x_axis = DateAxis(crossAx=100)
+    linechart_fw_creation_date.x_axis.number_format = 'd-mmm'
+    linechart_fw_creation_date.x_axis.majorTimeUnit = 'days'
+    linechart_fw_creation_date.x_axis.title = 'Date'
+    
+    # where to reference data from
+    ws2 = wb['Formatted']
+    data = Reference(ws2,
+                    min_col=2,
+                    min_row=2,
+                    max_col=2,
+                    max_row=ws2.max_row,
+                    )
+    dates = Reference(ws2,
+                    min_col=1,
+                    min_row=2,
+                    max_col=1,
+                    max_row=ws2.max_row,
+                    )
+    linechart_fw_creation_date.add_data(data, titles_from_data=True)
+    linechart_fw_creation_date.set_categories(dates)
 
-# where to reference data from
-data = Reference(ws2,
-                min_col=2,
-                min_row=2,
-                max_col=2,
-                max_row=ws2.max_row,
-                )
-dates = Reference(ws2,
-                  min_col=1,
-                  min_row=2,
-                  max_col=1,
-                  max_row=ws2.max_row,
-                  )
-linechart_fw_creation_date.add_data(data, titles_from_data=True)
-linechart_fw_creation_date.set_categories(dates)
-
-ws2.add_chart(linechart_fw_creation_date, 'C1')
-data_v.save(os.path.join('Data_Intern', 'dv_intern.xlsx'))
-
-
-
-# counter_ex = Counter(['2020-12-12','2020-12-12','2034-23-03'])
-# print(counter_ex)
-
-# df.to_excel(excel_writer='temp1.xlsx')
+    return linechart_fw_creation_date
 
 
+def save_chart(wb, linechart_fw_creation_date, output_file):
+    ws2 = wb['Formatted']
+    ws2.add_chart(linechart_fw_creation_date, 'C1')
+    wb.save(os.path.join('Data_Intern', output_file))
 
-# print(df)
 
+if __name__ == '__main__':
+    main()
 
 
 
