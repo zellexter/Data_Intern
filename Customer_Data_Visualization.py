1b56523c768275b65ce1c5be2995a6d0dd5a72e1
Michelle Mu <michellemjx@gmail.com>
2024-07-09 12:42:04 -0700

    # generalized draw_piechart() to use min/max col/row to find data # draw_piechart() now passes in chart name as output ws name # removed usecol= argument from import_data(), redundent as columns are found by str not index. all columns from input file are imported to df. # generalized for loops in main() so col_name points toward needed columns in df # format_data_linechart() passes in is_month with a default value of False. # generalize variables in format_data_piechart()

--- Customer_Data_Visualization.py
+++ Customer_Data_Visualization.py
@@ -13,6 +13,14 @@ import numpy as np
 from collections import Counter
 from datetime import date
 
+# COMMIT COMMENTS
+# generalized draw_piechart() to use min/max col/row to find data
+# draw_piechart() now passes in chart name as output ws name
+# removed usecol= argument from import_data(), redundent as columns are found by str not index. all columns from input file are imported to df.
+# generalized for loops in main() so col_name points toward needed columns in df
+# format_data_linechart() passes in is_month with a default value of False.
+# generalize variables in format_data_piechart() 
+
 
 # DONE visualize data into date line chart https://openpyxl.readthedocs.io/en/stable/charts/line.html
 # DONE line 62 linechart_fw_creation_date.y_axis.crossAx ????
@@ -29,15 +37,21 @@ from datetime import date
 #   DONE format_data() wb to ws, appends all formatted data into specified ws instead of wb
 #   DONE draw_line_chart() wb to ws, take data from specified ws instead of wb
 #   DONE save_chart() wb to ws, save chart into specified ws instead of wb
-# TODO generalize into class
-# TODO generalize format_data() to exclude pulling specific columns
-#   pass in which columns and where to put data
-#   pass in type of data and how to treat
+# DONE generalize into class
+# DONE generalize format_data() to exclude pulling specific columns
+#   DONE pass in which columns and where to put data
+#   DONE pass in type of data and how to treat
 # TODO create net type data visualization piechart
+#   TODO new function to add unoriginal data to df
+#       ??? how to approach: make new function or pass in original_data=True to format_data function
 #   DONE create general pie chart for mode types
-# TODO generalize reference columns, size of current data (len)
-#   TODO generalize piechart.title
-# TODO generalize for loops in main function so that you can pass in the needed columns of data from df
+# DONE generalize reference columns, size of current data (len)
+#   DONE generalize piechart.title
+# DONE generalize for loops in main function so that you can pass in the needed columns of data from df
+# TODO create VisualizationType() Class with all draw_chart functions and format data functions*** WAIT FOR DAD 
+# TODO generalize format data function to pass in type of chart, combine pie and line format data functions
+# TODO generalize draw chart function to pass in type of chart
+# TODO generalize main() for loops to pass in type of chart
 
 
 # STEP BY STEP
@@ -82,22 +96,23 @@ class DataVisualization():
         self.wb = self.prepare_wb()
 
         # create linechart
-        for col_name, output_worksheet in [
-            ('FwCreateDate', 'Daily Chart'),
-            ('FwCreateMonth', 'Monthly Chart'),
-        ]:
+        for col_name, output_worksheet, is_month in [
+            ('FwCreateDate', 'Daily Chart', False),
+            ('FwCreateMonth', 'Monthly Chart', True),
+            ]:
             ws = self.prepare_ws(output_worksheet)
-            self.format_data_linechart(df, ws, col_name)
+            self.format_data_linechart(df, ws, col_name, is_month)
             self.draw_linechart(ws)
 
         # create piechart
-        for col_name, output_worksheet in [
-            ('Mode', 'Mode Piechart'),
-            # ('CloudSec', 'CloudSec NetType Piechart'),
-            # ('Netsec', 'Netsec NetType Piechart'),
+        for col_name, output_worksheet,  in [
+            ('Mode', 'Mode Piechart', ),
+            # ('CloudSec', 'CloudSec NetType Piechart', ),
+            # ('Netsec', 'Netsec NetType Piechart', ),
+            # IN ORDER TO DO THESE^^ need to add data to df
         ]:
             ws = self.prepare_ws(output_worksheet)
-            self.format_data_piechart(df, ws, col_name)
+            self.format_data_piechart(df, ws, col_name, )
             self.draw_piechart(ws, chart_title=output_worksheet)
 
         self.save_wb()
@@ -108,7 +123,7 @@ class DataVisualization():
         Creates by month data column from date data. Returns the dataframe.'''
 
         # import xlsx file, Data sheet, columns for mode, fwcreatedate, and nettype
-        dict_df = pd.read_excel(io=self.input_file, sheet_name=['Data'], usecols=[1,36,37]) 
+        dict_df = pd.read_excel(io=self.input_file, sheet_name=['Data']) 
         df = dict_df['Data']
 
         df['FwCreateMonth'] = df['FwCreateDate'].str.slice(0,7)
@@ -124,39 +139,37 @@ class DataVisualization():
 
 
     def prepare_ws(self, ws_name): # pass in col_name as name for ws
-        '''This function creates a new worksheet in the workbook'''
+        '''This function creates a new worksheet named output_worksheet in workbook'''
         ws = self.wb.create_sheet(ws_name)
         return ws
 
 
-    def format_data_linechart(self, df, ws, col_name):
+    def format_data_linechart(self, df, ws, col_name, is_month=False):
         '''This function creates a visualization for date data'''
-        
-        # 3 seperate columns for mode, fwcreatedate, and nettype
-        mode = df.loc[:, 'Mode']
-        fw_create_date = df.loc[:, col_name]
-        net_type = df.loc[:,'NetType']
+
+        data = df.loc[:, col_name]
 
         # creates sorted_count_date list within list [date, count], adds header in formatted_date_data
-        count_date = Counter(list(fw_create_date))
-        count_date.pop(np.nan, None) # pop np.nan from data, if nonexistent then do nothing
+        count_data = Counter(list(data))
+        count_data.pop(np.nan, None) # pop np.nan from data, if nonexistent then do nothing
+
         # definition of when is_month is True
-        is_month = list(count_date.keys())[0].count('-') == 1
+        is_month = list(count_data.keys())[0].count('-') == 1
         # what to do when is_month is True
         if is_month:
-            count_date = {
+            count_data = {
                 (k + '-01') : v # what we are doing
-                for k, v in count_date.items() # for which items
+                for k, v in count_data.items() # for which items
                 if k is not np.nan # conditional # use is not for np.nan because we don't know if unknowns == unknowns, but we DO know if something IS NOT unknown
                 }
-        # list_count_date = count_date.items()
+        # list_count_data = count_data.items()
             
-        sorted_count_date = sorted([[date(*[int(item) for item in k.split('-')]),v] #what we are doing to the items
-                                for k,v in count_date.items() #where we find the items from
+        sorted_count_data = sorted([[date(*[int(item) for item in k.split('-')]),v] #what we are doing to the items
+                                for k,v in count_data.items() #where we find the items from
                                 if k is not np.nan] #conditional filter
                                 )
-        count_date_headers = [['Date','Count']]
-        formatted_date_data = count_date_headers + sorted_count_date
+        count_data_headers = [['Date','Count']]
+        formatted_date_data = count_data_headers + sorted_count_data
         # return formatted_date_data
 
         # appends formatted_date_data to ws2 only if ws2 is empty (to stop it from appending again every time the code is run)
@@ -166,35 +179,35 @@ class DataVisualization():
             
     def format_data_piechart(self, df, ws, col_name):
         '''This function creates visualization for a piechart'''
-        # TODO generalize to pass in any col_name
-    
 
-        # create column for mode data
-        mode = df.loc[:, col_name]
+        # create column for data
+        data = df.loc[:, col_name]
 
         # creates count_date
-        count_mode = Counter(list(mode))
-        count_mode.pop(np.nan, None) # pop np.nan from data, if nonexistent then do nothing
+        count_data = Counter(list(data))
+        count_data.pop(np.nan, None) # pop np.nan from data, if nonexistent then do nothing
 
         # total variable to assist in converting to percentage
         # Counter({'NetSec': 943, 'CloudSec': 723})
-        total_count = count_mode['NetSec'] + count_mode['CloudSec']
+        total_count = count_data['NetSec'] + count_data['CloudSec']
 
-        # list comp to sort mode types into usable format
-        sorted_count_mode = sorted([[k,v/total_count]
-                                    for k,v in count_mode.items()
+        # list comp to sort data types into usable format
+        sorted_count_data = sorted([[k,v/total_count]
+                                    for k,v in count_data.items()
                                     if k is not np.nan] # gets rid of np.nan
                                    )
-        count_mode_headers = [['Mode', 'Count']]
-        formatted_mode_data = count_mode_headers + sorted_count_mode
+        count_data_headers = [[col_name, 'Count']]
+        formatted_data = count_data_headers + sorted_count_data
         # return formatted_mode_data
 
         # appends formatted_mode_data to ws2 only if ws2 is empty (to stop it from appending again every time the code is run)
         if ws['A1'].value == None: # check ws2 is empty
-            for row_num, row_data in enumerate(formatted_mode_data, start=2):
+            for row_num, row_data in enumerate(formatted_data, start=2):
                 ws.append(row_data)
                 # convert to percentage
                 ws.cell(row=row_num, column=2).number_format = '0.0%'
+
+
 
 
     def draw_linechart(self, ws):
@@ -232,13 +245,11 @@ class DataVisualization():
 
     def draw_piechart(self, ws, chart_title):
         '''This function draws a pie chart and saves it into a cell'''
-        # TODO generalize reference columns, size of current data (len)
-        # TODO generalize piechart.title
         
         # creating pie chart
         piechart = PieChart()
-        labels = Reference(ws, min_col=1, min_row=3, max_row=4)
-        data = Reference(ws, min_col=2, min_row=2, max_row=4)
+        labels = Reference(ws, min_col=ws.min_column, min_row=3, max_row=ws.max_row)
+        data = Reference(ws, min_col=ws.max_column, min_row=2, max_row=ws.max_row)
         piechart.add_data(data, titles_from_data=True)
         piechart.set_categories(labels)
         piechart.title = chart_title
